c5a95a7b658b4f85947df588c2f042e1d9a640a9
monjo003 <monjo003@morris.umn.edu>
2018-04-04 23:15:47 -0500

    Added ResourceRequest Handler

--- server/src/main/java/umm3601/database/ResourceRequestHandler.java
+++ server/src/main/java/umm3601/database/ResourceRequestHandler.java
@@ -10,6 +10,40 @@ public class ResourceRequestHandler {
     public ResourceRequestHandler(ResourceController resourceController){
         this.resourceController = resourceController;
     }
+    /**Method called from Server when the 'api/resources/:id' endpoint is received.
+     * Get a JSON response with a list of all the users in the database.
+     *
+     * @param req the HTTP request
+     * @param res the HTTP response
+     * @return one user in JSON formatted string and if it fails it will return text with a different HTTP status code
+     */
+
+    // gets one resource using its ObjectId--didn't use, just for potential future functionality
+    public String getResourceJSON(Request req, Response res){
+        res.type("application/json");
+        String id = req.params("id");
+        String resource;
+        try {
+            resource = resourceController.getResource(id);
+        } catch (IllegalArgumentException e) {
+            // This is thrown if the ID doesn't have the appropriate
+            // form for a Mongo Object ID.
+            // https://docs.mongodb.com/manual/reference/method/ObjectId/
+            res.status(400);
+            res.body("The requested resource id " + id + " wasn't a legal Mongo Object ID.\n" +
+                "See 'https://docs.mongodb.com/manual/reference/method/ObjectId/' for more info.");
+            return "";
+        }
+        if (resource != null) {
+            return resource;
+        } else {
+            res.status(404);
+            res.body("The requested resource with id " + id + " was not found");
+            return "";
+        }
+    }
+
+
 
     /**Method called from Server when the 'api/resources' endpoint is received.
      * This handles the request received and the response
@@ -19,13 +53,59 @@ public class ResourceRequestHandler {
      * @return an array of users in JSON formatted String
      */
 
-    // Gets the goals from the DB given the query parameters
+    // Gets the resources from the DB given the query parameters
     public String getResources(Request req, Response res)
     {
-        //debugging code
-        //System.out.println("It went in RRHandler and did getResources()");
         res.type("application/json");
         return resourceController.getResources(req.queryMap().toMap());
+    }
+
+    /**Method called from Server when the 'api/users/new'endpoint is recieved.
+     * Gets specified user info from request and calls addNewUser helper method
+     * to append that info to a document
+     *
+     * @param req the HTTP request
+     * @param res the HTTP response
+     * @return a boolean as whether the user was added successfully or not
+     */
+    public String addNewResource(Request req, Response res)
+    {
+
+        res.type("application/json");
+        Object o = JSON.parse(req.body());
+        try {
+            // if the object that is the JSON representation of the request body's class is the class BasicDBObject
+            // then try to add the resource with resourceController's addNewResource method
+            if(o.getClass().equals(BasicDBObject.class))
+            {
+                try {
+                    BasicDBObject dbO = (BasicDBObject) o;
+
+                    String title = dbO.getString("title");
+                    String link = dbO.getString("link");
+
+
+                    System.err.println("Adding new resource [title=" + title + ", link=" + link +']');
+                    return resourceController.addNewResource(title, link).toString();
+                }
+                catch(NullPointerException e)
+                {
+                    System.err.println("A value was malformed or omitted, new resource request failed.");
+                    return null;
+                }
+
+            }
+            else
+            {
+                System.err.println("Expected BasicDBObject, received " + o.getClass());
+                return null;
+            }
+        }
+        catch(RuntimeException ree)
+        {
+            ree.printStackTrace();
+            return null;
+        }
     }
 
 }
